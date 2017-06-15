43a07a70790fa44e46aa3845a348c842b0bc9036
parelhutahaean <parel.hutahaean@gmail.com>
2017-06-15 16:15:49 +0700

    Third Commit

--- client/src/components/MainPage.vue
+++ client/src/components/MainPage.vue
@@ -12,12 +12,35 @@
         <b-collapse is-nav id="nav_collapse">
 
           <b-nav is-nav-bar class="ml-auto" right>
-            <b-nav-item v-if="isLogin" >Sign Out</b-nav-item>
-            <b-nav-item v-if="!isLogin" >Sign In</b-nav-item>
-            <b-nav-item v-if="!isLogin" >Sign Up</b-nav-item>
+            <b-nav-item v-if="isLogin">Sign Out</b-nav-item>
+            <b-nav-item v-if="!isLogin"  @click="$root.$emit('show::modal','modalSignIn')">Sign In</b-nav-item>
+            <b-nav-item v-if="!isLogin"  @click="$root.$emit('show::modal','modalSignUp')">Sign Up</b-nav-item>
           </b-nav>
         </b-collapse>
       </b-navbar>
+    </div>
+    <div>
+      <div>
+        <b-modal id="modalSignIn" title="Sign In" @ok="submit" @shown="clearName">
+
+          <form @submit.stop.prevent="submit">
+            <b-form-input type="text" placeholder="Enter username" v-model="inusername"></b-form-input><br>
+            <b-form-input type="password" placeholder="Enter password" v-model="inpassword"></b-form-input>
+          </form>
+
+        </b-modal>
+      </div>
+      <div>
+        <b-modal id="modalSignUp" title="Sign Up" @ok="submit" @shown="clearName">
+
+          <form @submit.stop.prevent="submit">
+            <b-form-input type="text" placeholder="Enter username" v-model="outusername"></b-form-input><br>
+            <b-form-input type="password" placeholder="Enter password" v-model="outpassword"></b-form-input>
+          </form>
+
+        </b-modal>
+      </div>
+
     </div>
   </div>
 </template>
@@ -34,6 +57,12 @@ export default {
   methods: {
     getArticles () {
       var self = this;
+      console.log('Hello');
+      // this.$http.get('http://localhost:3000/api/articles/')
+      // .then(result => {
+      //   console.log(result);
+      //   self.articles = result.body
+      // })
       axios.get('http://localhost:3000/api/articles/')
       .then(result => {
         console.log(result);
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import MainPage from '@/components/MainPage'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
 
-Vue.use(Router)
+Vue.use(VueAxios, axios);
+Vue.use(BootstrapVue);
+Vue.use(Router);
 
 export default new Router({
   routes: [
